e8226b14cc1e208a29a327a3db2a1eabd93d6b03
Rohith Murali <rohithmurali11@protonmail.com>
2026-05-19 23:10:03 +0530

    changed db from localhost to cloud

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 5000;
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/expense-manager';
-export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
-export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Rohith:Rohith@cluster0.gligv3f.mongodb.net/expense-manager';
+export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || "qwerty";
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "asdfgh";
 export const JWT_ACCESS_EXPIRY = process.env.JWT_ACCESS_EXPIRY || '15m';
 export const JWT_REFRESH_EXPIRY = process.env.JWT_REFRESH_EXPIRY || '7d';
